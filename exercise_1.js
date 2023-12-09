11b92b64691facc3fe42a26018cda90564280d31
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-12-08 16:06:37 -0800

    Update exercise_1.js

--- exercise_1.js
+++ exercise_1.js
@@ -41,7 +41,7 @@
 // ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
 // We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. 
 // Your array should contain at least one string, one number, and one boolean.
-let yourArray; ["a", 2, true, "c", null,{name: "john"}] // Change this line
+let yourArray = ["a", 2, true, "c", null,{name: "john"}] // Change this line
 
 
 // let yourArray; // Change this line
