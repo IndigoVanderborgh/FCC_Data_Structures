d2f9fb61d48bd2395a90606202296a16a9db2910
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-12-10 16:49:13 -0800

    Update exercise_4.js

--- exercise_4.js
+++ exercise_4.js
@@ -28,12 +28,13 @@
 
 // We have defined a function, popShift, which takes an array as an argument and returns a new array. Modify the function, using pop() and shift(), to remove the first and last elements of the argument array, and assign the removed elements to their corresponding variables, so that the returned array contains their values.
 function popShift(arr) {
-    let popped; // Change this line
-    let shifted; // Change this line
-    return [shifted, popped];
-  }
-  
-  console.log(popShift(['challenge', 'is', 'not', 'complete']));
+  let popped = arr.pop();
+  let shifted = arr.shift();
+  return [shifted, popped];
+}
+
+// do not change code below this line
+console.log(popShift(["challenge", "is", "not", "complete"]));
 
 //   function popShift(arr) {
 //     let popped; // Change this line
