3d4178c05a108283c69786ea48332e2c6c82e45b
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-12-12 18:04:13 -0800

    Update exercise_7.js

--- exercise_7.js
+++ exercise_7.js
@@ -14,6 +14,23 @@
 
 
 // ------------------------------------------ exercise ------------------------------------------------------------------------------------------------------------------------------
+// We have defined a function, forecast, 
+// that takes an array as an argument. Modify the function using slice() to extract information from the argument array and return a new array that contains the string elements warm and sunny.
+function forecast(arr) {
+    // Only change code below this line
 
+    return arr;
+}
 
+  // Only change code above this line
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+// function forecast(arr) {
+//     // Only change code below this line
+
+//     return arr;
+//   }
+
+//   // Only change code above this line
+//   console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
 // ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
