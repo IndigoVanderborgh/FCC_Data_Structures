a776505382d921165c78b04f44ea62358079677f
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-12-18 19:34:05 -0800

    Update exercise_7.js

--- exercise_7.js
+++ exercise_7.js
@@ -35,3 +35,8 @@ console.log(
 //   // Only change code above this line
 //   console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
 // ------------------------------------------ Additional information ----------------------------------------------------------------------------------------------------------------
+
+// Let us look at the answer and diseect it:
+//return arr.slice(2, 4);
+//return            arr        .         slice           (2,                4);
+// ^return command  ^the array .         ^slice command   ^
