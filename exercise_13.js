89d5c1cc21cc1494175ef78ed943f075f88229bf
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-12-22 15:46:24 -0800

    Update exercise_13.js

--- exercise_13.js
+++ exercise_13.js
@@ -53,7 +53,14 @@
 //   };
   
 //   // Only change code below this line
-  
+// dot notation
+foods.bananas = 13
+foods.grapes = 35
+foods.strawberries = 27
+// bracket notation
+// foods['bananas'] = 13
+// foods['grapes'] = 35
+// foods['strawberries'] = 27
 //   // Only change code above this line
   
 //   console.log(foods);
